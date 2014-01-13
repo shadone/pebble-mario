87d7615791f1d8019cd3f23cfb51a318c156e4b2
Denis Dzyubenko <shadone@gmail.com>
2014-01-13 14:27:09 +0100

    Bump version number to 1.2
    Lazy me hasn't released the patch from back in 2013.

--- src/pebble-mario.c
+++ src/pebble-mario.c
@@ -26,7 +26,7 @@
 #define MY_UUID { 0xB6, 0xA0, 0xAB, 0x5F, 0x92, 0xB7, 0x4C, 0x2B, 0xBC, 0x0F, 0x34, 0x6B, 0x99, 0xAE, 0x30, 0xA0 }
 PBL_APP_INFO(MY_UUID,
              "Mario", "Denis Dzyubenko",
-             1, 0, /* App version */
+             1, 2, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_WATCH_FACE);
 
